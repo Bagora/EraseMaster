39f214b883c95b94fd5e9b90975b80fbabadeaf3
bagora <adityabagora@gmail.com>
2024-03-28 15:34:02 +0530

    depfix

--- app.py
+++ app.py
@@ -1,22 +1,23 @@
-from flask import Flask, render_template, request, send_file
+from flask import Flask, render_template, request
 from rembg import remove
 from PIL import Image
 from io import BytesIO
+import base64
 
 app = Flask(__name__)
 
 app.config["MAX_CONTENT_LENGTH"] = 5 * 1024 * 1024  # 5MB
 
-def convert_image(img):
-    buf = BytesIO()
-    img.save(buf, format="PNG")
-    byte_im = buf.getvalue()
-    return byte_im
-
 def fix_image(upload):
     image = Image.open(upload)
     fixed = remove(image)
     return fixed
+
+def convert_to_base64(img):
+    buffered = BytesIO()
+    img.save(buffered, format="PNG")
+    img_str = base64.b64encode(buffered.getvalue()).decode('utf-8')
+    return img_str
 
 @app.route('/')
 def index():
@@ -30,9 +31,8 @@ def upload():
             return "The uploaded file is too large. Please upload an image smaller than 5MB.", 400
         else:
             fixed_image = fix_image(upload_file)
-            return send_file(BytesIO(convert_image(fixed_image)),
-                             download_name="object-img.png",
-                             mimetype="image/png")
+            converted_image_base64 = convert_to_base64(fixed_image)
+            return render_template('index.html', converted_image=converted_image_base64)
 
 if __name__ == '__main__':
-    app.run(port = 8080)
+    app.run()
